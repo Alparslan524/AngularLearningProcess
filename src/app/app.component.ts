0bda9271d60eef8af1ab9443c5a1385f0dcf0451
Alparslan <alparslanaydgn02@gmail.com>
2023-03-29 22:11:25 +0300

    The fourth lesson is over. Binding Types Learned and Noted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',//Componentin kimliği gibi. Başka bir yerde çağırırken <app-root></app-root> kullanarak çağıracak ve component çalışacak
+  selector: 'app-root', //Componentin kimliği gibi. Başka bir yerde çağırırken <app-root></app-root> kullanarak çağıracak ve component çalışacak
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'AngularLearningProcess';
-// Anguların backendi gibi düşünebiliriz
+  //Anguların backendi gibi düşünebiliriz
+
+  sendValue: string = 'Property Binding(Sented Value)';
+  //Buradaki sendValue değişkenini Property Binding yaparak HomeComponent'ine gönderdik. Html kısmında app-home içinde köşeli parantezle çağırdık
+
+  btnText: string = 'Not Clicked';
+  btnClick() {
+    this.btnText = 'Clicked!!!';
+  }
+
+  //Two-Way data binding
+  twoWayData: string;
+
+  disabled : boolean = true;
+
+  bgColor : string="black";
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,12 @@
+import { Input } from '@angular/core';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
+  @Input() pageName: string;
+  // pageName dışarıdan yani başka componentten değer alıcak
 }
